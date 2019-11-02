a976edd54d5d9f333d8ab654bf82d173cc42902d
Andrew Rabon <andrewrabon@gmail.com>
2019-11-02 00:11:30 -0400

    Updated icons.

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,9 +5,9 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/andrewrabon/Code/chipmusic-fm/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/home/andrewrabon/Code/chipmusic-fm/src/pages/404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/home/andrewrabon/Code/chipmusic-fm/src/pages/index.js"))),
-  "component---src-pages-settings-js": hot(preferDefault(require("/home/andrewrabon/Code/chipmusic-fm/src/pages/settings.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/andrewrabon/chipmusic-fm/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/home/andrewrabon/chipmusic-fm/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/home/andrewrabon/chipmusic-fm/src/pages/index.js"))),
+  "component---src-pages-settings-js": hot(preferDefault(require("/home/andrewrabon/chipmusic-fm/src/pages/settings.js")))
 }
 
