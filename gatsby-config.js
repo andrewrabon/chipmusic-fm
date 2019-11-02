1bf1db49f89a84e3d74816fc3a807c48d4757c5a
Andrew Rabon <andrewrabon@gmail.com>
2019-11-02 00:24:46 -0400

    Updated icon paths.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,50 +1,50 @@
 module.exports = {
   siteMetadata: {
-    title: `ChipMusic.fm`,
-    description: `A simple shuffle-only music player for ChipMusic.org.`,
-    author: `@andrewrabon`,
+    title: 'ChipMusic.fm',
+    description: 'A simple shuffle-only music player for ChipMusic.org.',
+    author: '@andrewrabon',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `ChipMusic.fm`,
-        short_name: `ChipMusic.fm`,
-        start_url: `/`,
-        background_color: `#000000`,
-        theme_color: `#000000`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'ChipMusic.fm',
+        short_name: 'ChipMusic.fm',
+        start_url: '/',
+        background_color: '#000000',
+        theme_color: '#000000',
+        display: 'minimal-ui',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
+    'gatsby-plugin-offline',
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: 'gatsby-plugin-prefetch-google-fonts',
       options: {
         fonts: [
           {
-            family: `Josefin Sans`,
-            variants: [`bold`, `italic`]
+            family: 'Josefin Sans',
+            variants: ['bold', 'italic'],
           },
           {
-            family: `Roboto`,
-            subsets: [`latin`]
+            family: 'Roboto',
+            subsets: ['latin'],
           },
         ],
         display: 'swap',
       },
     },
   ],
-}
+};
