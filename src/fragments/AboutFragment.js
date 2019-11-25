d2c416b3b1edeaa50daa07f8e47c61230f799f3b
Andrew Rabon <andrewrabon@gmail.com>
2019-11-25 16:02:20 -0500

    About page more filled out.

--- src/fragments/AboutFragment.js
+++ src/fragments/AboutFragment.js
@@ -3,7 +3,19 @@ import React from 'react';
 export const AboutFragment = () => (
   <>
     <h1>About ChipMusic.fm</h1>
-    <p>TBD.</p>
+    <p>
+      ChipMusic.fm is a shuffle-only music player for the 8000+ free
+      {' '}
+      <a href="https://en.wikipedia.org/wiki/Chiptune" target="_blank" rel="noopener noreferrer">chiptunes</a>
+      {' '}
+      available on
+      {' '}
+      <a href="https://chipmusic.org/" target="_blank" rel="noopener noreferrer">ChipMusic.org</a>
+      , and it was created by
+      {' '}
+      <a href="https://andrewrabon.com/" target="_blank" rel="noopener noreferrer">Andrew Rabon</a>
+      .
+    </p>
 
     <h1>Disclosures and policies</h1>
     <p>
@@ -16,12 +28,25 @@ export const AboutFragment = () => (
       you should be aware this site uses cookies. 🍪
     </p>
     <p>
-      <a href="https://gist.github.com/andrewrabon/c02a6969fabb6772044c77e3f34c99cb" target="_blank" rel="noopener noreferrer">You can view the full ChipMusic.fm privacy policy here.</a>
+      You can view the full ChipMusic.fm privacy policy
+      {' '}
+      <a href="https://gist.github.com/andrewrabon/c02a6969fabb6772044c77e3f34c99cb" target="_blank" rel="noopener noreferrer">here</a>
+      .
     </p>
 
     <h1>Licenses and copyright</h1>
     <p>
-      The source code behind ChipMusic.fm is licensed under the
+      The
+      {' '}
+      <a
+        href="https://github.com/andrewrabon/chipmusic-fm"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        source code behind ChipMusic.fm
+      </a>
+      {' '}
+      is licensed under the
       {' '}
       <a
         href="https://github.com/andrewrabon/chipmusic-fm/blob/master/LICENSE"
@@ -30,8 +55,14 @@ export const AboutFragment = () => (
       >
         Mozilla Public License, version 2.0
       </a>
-      . ChipMusic.fm itself is &copy;2017-2019 Andrew Rabon.
+      . ChipMusic.fm itself is &copy; 2017-2019 Andrew Rabon.
     </p>
-    <p>Songs played on ChipMusic.fm have been generously made available under Creative Commons.</p>
+    <p>
+      Songs played on ChipMusic.fm have been generously made available under Creative Commons,
+      mostly as
+      {' '}
+      <a href="https://creativecommons.org/licenses/by-nc-nd/3.0/" target="_blank" rel="noopener noreferrer">BY-NC-ND</a>
+      .
+    </p>
   </>
 );
